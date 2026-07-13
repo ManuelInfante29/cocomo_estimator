c056e2deeb6804d93d4184bef34ccae9aeb1dee4
ManuelInfante29 <darbaster.2000@gmail.com>
2026-07-12 23:40:44 -0500

    Actualizacion
    Probando pagina

--- app.py
+++ app.py
@@ -156,5 +156,11 @@ def exportar_pdf_fp():
                      as_attachment=True, download_name=filename)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
